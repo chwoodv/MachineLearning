192b146f8882769a783c559015b097dc90ad5946
chwoodv <148597237+chwoodv@users.noreply.github.com>
2025-06-24 15:58:32 -0500

    Cleaned up imports

--- Scripts/Flowers/algorithm.py
+++ Scripts/Flowers/algorithm.py
@@ -2,14 +2,14 @@ print("Running algorithm.py...")
 
 # Load libraries
 from pandas import read_csv
-from pandas.plotting import scatter_matrix
+# from pandas.plotting import scatter_matrix
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
+# from sklearn.metrics import classification_report
+# from sklearn.metrics import confusion_matrix
+# from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
